8c7e3cefd81d7274d9e621bbbb2fc5f36ea576a5
energyfirefox <energyfirefox@gmail.com>
2014-03-12 13:00:12 +0200

    graphs modified

--- r_graphs.R
+++ r_graphs.R
@@ -14,9 +14,16 @@ metrics$skin_temp <- f_to_celsius(metrics$skin_temp)
 
 metrics2 <- metrics[as.Date(metrics$time) == "2014-03-04", ]
 
-plot(metrics2$time, metrics2$steps, col = "red", pch = 21, cex = 0.4)
-points(metrics2$time, metrics2$heart_rate, col = "blue", pch = 21, cex = 0.4)
+plot(metrics2$time, metrics2$steps, col = "red", pch = 19, cex = 0.6, xlab = "Time", ylab = "")
+# add grid lines
+grid(nx = 26, ny = 26, col = "grey", lty = "dotted", lwd = par("lwd"), equilogs = TRUE)
+# add more points
+points(metrics2$time, metrics2$heart_rate, col = "blue", pch = 19, cex = 0.4)
 points(metrics2$time, metrics2$skin_temp, col = "green", pch = 21, cex = 0.4)
+# add legend
+legend(2000, 9.5, c("Steps", "Heart rate", "Skin temperature"), lty = c(1, 1, 1), col = c("red", "blue", "green"))
+
+
 
 
 activities$time1 <- strptime(as.character(activities$time1), format = "%A %b %d %H:%M:%S %Y")
@@ -26,12 +33,12 @@ activities2 <- activities[as.Date(activities$time1) == "2014-03-04", ]
 
 plot(activities2$time1, activities2$fake_val, type = "l", ylim = c(0, 200))
 for (i in 1:nrow(activities2)){
-  cur_col <- gray((as.integer(activities2$bodystate[i])+2)/8)
+  cur_col <- gray((as.integer(activities2$bodystate[i]))/8)
   rect(activities2$time1[i], 0, activities2$time2[i], 200, col = cur_col)
 }
 
 points(metrics2$time, metrics2$steps, col = "blue", pch = 19, cex = 0.6)
-points(metrics2$time, metrics2$heart_rate, col = "red", pch = 19, cex = 0.5)
+points(metrics2$time, metrics2$heart_rate, col = "red", pch = 21, cex = 0.5)
 points(metrics2$time, metrics2$skin_temp, col = "green", pch = 19, cex = 0.2)
 
 
